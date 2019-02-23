8ff7d145f99a630b1306318609719e2871d7c518
ElegantDalek <karatedkid@gmail.com>
2019-02-23 17:21:04 -0600

    Added visual list updates

--- animation_maker/animation_maker/visual_list.py
+++ animation_maker/animation_maker/visual_list.py
@@ -6,26 +6,16 @@ class Visual_List(object):
         self.data = data
         self.x = x
         self.y = y
-        
+
         self.size = size
-        self.gap=5
-        
-        # adding all variable elements 
-        self.list_vars = []
-        for i, item in enumerate(data):
-            var_name = ''
-            if i == len(data) - 1:
-                var_name = name
-            self.list_vars.append(Variable(var_name, item, x, y + i * (size + gap)))
+        self.gap = gap
     
     def draw(self):
-        for i, var in enumerate(self.list_vars):
-            var.set_coords(self.x, self.y + i * (self.size + self.gap))
-            var.draw()
-            
-    # def update_list(self, new_data):
-    #     for i, item in enumerate(new_data):
-    #         var_name = ''
-    #         if i == len(data) - 1:
-    #             var_name = name
-    #         self.list_vars.append(Variable(var_name, item, x, y + i * (size + gap)))
+        for i, item in enumerate(self.data):
+            var_name = ''
+            if i == len(self.data) - 1:
+                var_name = self.name
+            Variable(var_name, item, self.x, self.y + (self.size + self.gap) * i).draw()
+    
+    def update_list(self, new_data):
+        self.data = new_data
