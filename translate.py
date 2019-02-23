f764feeda89de88ddeda259f57a031896b75cc25
Qwertazertl0 <42043289+Qwertazertl0@users.noreply.github.com>
2019-02-23 11:37:37 -0600

    translate.py updated

--- translate.py
+++ translate.py
@@ -3,6 +3,17 @@ import re
 
 def translate(line):
     '''
+    Translate all lines of code to be able to be logged
+    @param line: string representing a line of code
+    '''
+    if re.compile('=').search(line):
+        return replace_assign(line)
+
+    #Other stuff
+
+
+def replace_assign(line):
+    '''
     Replace line containing assignment operator with an assignment object
     @param line: string representing a line of code
     '''
@@ -17,7 +28,8 @@ def translate(line):
     for i in range(len(left_obj)):
         left_obj[i] = left_obj[i].strip()
         right_obj[i] = right_obj[i].strip()
-        assignment_li.append(left_obj[i] + " = " + "Variable(" + right_obj[i] + ")")
+        assignment_li.append(left_obj[i] + " = "
+                + "variable(" + left_obj[i] + "," + right_obj[i] + ")")
 
     return assignment_li
 
@@ -25,5 +37,6 @@ def test_translate():
     print(translate("a = b"))
     print(translate("value = 5"))
     print(translate("p, q, r ,k,c= 1, 2, 'c', arg, getFunc()"))
+    print(translate(" b = a + 1"))
 
 test_translate()
