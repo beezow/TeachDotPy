b82e7e5d9bef2f2bc3629b3bee6329355ad939a4
Andrew Fei <afei3@illinois.edu>
2019-02-23 15:56:47 -0600

    Reformatted parser to work with json file from the logger

--- animation_maker/animation_maker/json_parser.py
+++ animation_maker/animation_maker/json_parser.py
@@ -2,34 +2,46 @@ import json
 import ast
 
 class Stack:
-    def __init__(self, name, type, data):
-        self.name = name
-        self.type = type
-        self.data = ast.literal_eval(data)
-
-class Int:
-    def __init__(self, name, type, data):
+    def __init__(self, name, type, data, index):
         self.name = name
         self.type = type
         self.data = data
+        self.index = index
 
-class List:
-    def __init__(self, name, type, data):
+class Int:
+    def __init__(self, name, type, data, index):
         self.name = name
         self.type = type
-        self.data = ast.literal_eval(data)
+        self.data = data
+        self.index = index
+
+class String:
+    def __init__(self, name, type, data, index):
+        self.name = name
+        self.type = type
+        self.data = data
+        self.index = index
+
+class List:
+    def __init__(self, name, type, data, index):
+        self.name = name
+        self.type = type
+        self.data = data
+        self.index = index
 
 def list_of_objects():
-    with open('test.json') as json_file:
+    with open('test1DList.json') as json_file:
         data = json.load(json_file)
         objects = []
         for p in data["steps"]:
-            if p["type"] == "stack":
-                objects.append(Stack(p["name"], p["type"], p["data"]))
-            if p["type"] == "list":
-                objects.append(List(p["name"], p["type"], p["data"]))
-            if p["type"] == "int":
-                objects.append(Int(p["name"], p["type"], p["data"]))
+            if p["type"] == "<class 'stack'>":
+                objects.append(Stack(p["name"], p["type"], p["data"], p["index"]))
+            if p["type"] == "<class 'list'>":
+                objects.append(List(p["name"], p["type"], p["data"], p["index"]))
+            if p["type"] == "<class 'int'>":
+                objects.append(Int(p["name"], p["type"], p["data"], p["index"]))
+            if p["type"] == "<class 'string'>":
+                objects.append(Int(p["name"], p["type"], p["data"], p["index"]))
         return objects
 
 list_of_objects()
