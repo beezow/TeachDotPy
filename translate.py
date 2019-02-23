e2dacc15000969585c99fa7eaa422e608b195016
Qwertazertl0 <42043289+Qwertazertl0@users.noreply.github.com>
2019-02-23 02:40:35 -0600

    translate.py added

---
+++ translate.py
@@ -0,0 +1,29 @@
+#Method file for translating native python code
+import re
+
+def translate(line):
+    '''
+    Replace line containing assignment operator with an assignment object
+    @param line: string representing a line of code
+    '''
+    split_line = line.split("=")
+    left = split_line[0]
+    right = split_line[1]
+
+    assignment_li = []
+    left_obj = left.split(",")
+    right_obj = right.split(",")
+
+    for i in range(len(left_obj)):
+        left_obj[i] = left_obj[i].strip()
+        right_obj[i] = right_obj[i].strip()
+        assignment_li.append(left_obj[i] + " = " + "Variable(" + right_obj[i] + ")")
+
+    return assignment_li
+
+def test_translate():
+    print(translate("a = b"))
+    print(translate("value = 5"))
+    print(translate("p, q, r ,k,c= 1, 2, 'c', arg, getFunc()"))
+
+test_translate()
