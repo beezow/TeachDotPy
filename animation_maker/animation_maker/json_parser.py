cbb3e551a11ae58627b0aece824d45ad519da6ed
Andrew Fei <afei3@illinois.edu>
2019-02-23 15:25:15 -0600

    Data takes string list and parses it as an actual list

--- animation_maker/animation_maker/json_parser.py
+++ animation_maker/animation_maker/json_parser.py
@@ -1,10 +1,11 @@
 import json
+import ast
 
 class Stack:
     def __init__(self, name, type, data):
         self.name = name
         self.type = type
-        self.data = data
+        self.data = ast.literal_eval(data)
 
 class Int:
     def __init__(self, name, type, data):
@@ -16,17 +17,19 @@ class List:
     def __init__(self, name, type, data):
         self.name = name
         self.type = type
-        self.data = data
+        self.data = ast.literal_eval(data)
 
-with open('test.json') as json_file:
-    data = json.load(json_file)
-    objects = []
-    for p in data["steps"]:
-        if p["type"] == "stack":
-            objects.append(Stack(p["name"], p["type"], p["data"]))
-        if p["type"] == "list":
-            objects.append(List(p["name"], p["type"], p["data"]))
-        if p["type"] == "int":
-            objects.append(Int(p["name"], p["type"], p["data"]))
-    for q in objects:
-        print(q.data)
+def list_of_objects():
+    with open('test.json') as json_file:
+        data = json.load(json_file)
+        objects = []
+        for p in data["steps"]:
+            if p["type"] == "stack":
+                objects.append(Stack(p["name"], p["type"], p["data"]))
+            if p["type"] == "list":
+                objects.append(List(p["name"], p["type"], p["data"]))
+            if p["type"] == "int":
+                objects.append(Int(p["name"], p["type"], p["data"]))
+        return objects
+
+list_of_objects()
