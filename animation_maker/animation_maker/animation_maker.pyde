d1e7009cfc57acaa0d758b87e01fe4ffe5dd09a6
ElegantDalek <karatedkid@gmail.com>
2019-02-23 13:26:25 -0600

    Basic variable class implemented

--- animation_maker/animation_maker/animation_maker.pyde
+++ animation_maker/animation_maker/animation_maker.pyde
@@ -1,6 +1,15 @@
+import json
+from variable import *
+var = None
 def setup():
+    global var
     size(720, 480)
+    with open('test.json') as f:
+        data = json.load(f)
+        print(data)
+    
+    var = Variable('counter', '1', 255, 255)
     
 def draw():
     background(255)
-    
+    var.draw(color(44, 218, 240))
---
+++ animation_maker/animation_maker/variable.py
@@ -0,0 +1,19 @@
+class Variable(object):
+    def __init__(self, name, data, x, y, size=40):
+        self.name = name
+        self.data = data
+        self.x = x
+        self.y = y
+        self.size = size
+        
+    def draw(self, color):
+        TEXTSIZE = 18
+        fill(color)
+        rect(self.x, self.y, self.size, self.size)
+        fill(0)
+        #text(str(self.data), self.x self.y + TEXTSIZE / 2)
+        textSize(18)
+        text(self.name, self.x, self.y + self.size + TEXTSIZE)
+    
+    def update_data(self, data):
+        self.data = data
