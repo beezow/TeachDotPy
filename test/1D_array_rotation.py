3292cbbf3769276d781b112576c1964f5ebcad49
yiliang6 <yiliang6@illinois.edu>
2019-02-23 23:36:04 -0600

    modified 1D_array_rotation.py for better test case

--- test/1D_array_rotation.py
+++ test/1D_array_rotation.py
@@ -1,6 +1,6 @@
 arr = [1,2,3,4,5]
 answer = []
-shift = 1
+
 for i in range(len(arr)):
-    answer.append(arr[(i + shift) % len(arr)])
+    answer.append(arr[(i + 1) % len(arr)])
     print(answer[i])
