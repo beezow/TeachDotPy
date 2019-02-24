e4314ed59884e7dd6e500a7afd2d8ad9bed20d00
drew <zbeezow@gmail.com>
2019-02-23 23:56:41 -0600

    remove old test

--- test_sample_code/1D_list_iterator.py
+++
@@ -1,5 +0,0 @@
-
-c = [0,0,0,0,0,0,0]
-for i in range(len(c)):
-    c[i] = i
-print(c)
--- test_sample_code/for_loops.py
+++
@@ -1,8 +0,0 @@
-a = [1,2,3,4]
-veryveryverylongvariablename = 0
-
-for i in a:
-    i += 1
-
-for i in range(10):
-    veryveryverylongvariablename += i
