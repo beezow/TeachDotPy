ffcddc9abd552c24725d62fc7ceb67e40ff74391
Andrew Fei <afei3@illinois.edu>
2019-02-24 03:25:32 -0600

    help

--- animation_maker/animation_maker/animation_maker.pyde
+++ animation_maker/animation_maker/animation_maker.pyde
@@ -15,7 +15,7 @@ def setup():
 
     object_tracker = []
     
-    another_list = list_of_objects("log/teach.json")
+    another_list = list_of_objects("../../test/spiral_matrix.json")
     for p in another_list:
         if p in object_tracker:
             replaceIndex = object_tracker.index(p)
--- animation_maker/animation_maker/json_parser.py
+++ animation_maker/animation_maker/json_parser.py
@@ -13,17 +13,20 @@ def list_of_objects(file_name):
             if p["type"] == "<collections.deque>":
                 objects.append(Visual_List(p["name"], p["type"], p["data"], p["index"]))
             if p["type"] == "<class 'list'>":
-                objects.append(Visual_List(p["name"], p["data"], 0, 0))
+                if isinstance(p["data"][0], list):
+                    print(p["data"][0])
+                else:
+                    objects.append(Visual_List(p["name"], p["data"], 0, 0))
             if p["type"] == "<class 'int'>":
                 if check_array_access(p["name"]):
                     list_name_stripped = p["name"].split('[')[0]
                     index_name = re.findall("(?<=\[)[^\]+]", p["name"])
-                    
+
                     to_modify = get_item(objects, list_name_stripped)
                     index_var = get_item(objects, index_name)
                     #print(index_name)
                     #print(to_modify.data)
-                    
+
                     copied_array = copy.deepcopy(to_modify.data)
                     copied_array[index_var.data] = p["data"]
 
@@ -34,7 +37,7 @@ def list_of_objects(file_name):
             if p["type"] == "<class 'string'>":
                 objects.append(Variable(p["name"], p["data"], 100, 100))
         return objects
-    
+
 def check_array_access(string):
     ind = re.findall("(?<=\\w)\[.+\]$", string)
     if len(ind) == 0:
@@ -42,7 +45,16 @@ def check_array_access(string):
     else:
         indices = ind[0]
         return len(re.findall("\[\]", indices)) == 0
-    
+
+def two_dimensional_array(two_dimensional):
+    returnArray = []
+    for i in two_dimensional["data"]:
+        row = []
+        for j in i:
+            row.append(Variable("", j))
+        returnArray.append(row)
+    return returnArray
+
 def get_item(objects, name):
     '''
     Takes a list of objects and returns one with the correct name
