e430bcbdb2b8419d18a253d6578da8ee6e053186
ElegantDalek <karatedkid@gmail.com>
2019-02-23 23:11:31 -0600

    readding json_parser

--- animation_maker/animation_maker/json_parser.py
+++ animation_maker/animation_maker/json_parser.py
@@ -1,5 +1,6 @@
 import json
 import ast
+import copy
 import re
 from visual_list import *
 from variable import *
@@ -14,7 +15,40 @@ def list_of_objects(file_name):
             if p["type"] == "<class 'list'>":
                 objects.append(Visual_List(p["name"], p["data"], 0, 0))
             if p["type"] == "<class 'int'>":
-                objects.append(Variable(p["name"], p["data"], 100, 45 * p["data"]))
+                if check_array_access(p["name"]):
+                    list_name_stripped = p["name"].split('[')[0]
+                    index_name = re.findall("(?<=\[)[^\]+]", p["name"])
+                    
+                    to_modify = get_item(objects, list_name_stripped)
+                    index_var = get_item(objects, index_name)
+                    print(to_modify.data)
+                    
+                    copied_array = copy.deepcopy(to_modify.data)
+                    #print(copied_array)
+                    #print(list_name_stripped, "<class 'list'>", copied_array, p["index"])
+                    objects.append(Visual_List(list_name_stripped, "<class 'list'>", copied_array, p["index"]))
+                    print(copied_array)
+                else:
+                    objects.append(Variable(p["name"], p["data"], 100, 45 * p["data"]))
             if p["type"] == "<class 'string'>":
                 objects.append(Variable(p["name"], p["data"], 100, 100))
         return objects
+    
+def check_array_access(string):
+    ind = re.findall("(?<=\\w)\[.+\]$", string)
+    if len(ind) == 0:
+        return False
+    else:
+        indices = ind[0]
+        return len(re.findall("\[\]", indices)) == 0
+    
+def get_item(objects, name):
+    '''
+    Takes a list of objects and returns one with the correct name
+    '''
+    for i in range(len(objects) - 1, -1, -1):
+        if isinstance(name, list):
+            name = name[0]
+        if objects[i].name == name:
+            return objects[i]
+    return None
