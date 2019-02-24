5f7839a762ba7a0221da9b8a504abfc465d14947
Qwertazertl0 <42043289+Qwertazertl0@users.noreply.github.com>
2019-02-24 02:14:25 -0600

    minor changes to resizing, variable obj more flexible

--- animation_maker/animation_maker/json_parser.py
+++ animation_maker/animation_maker/json_parser.py
@@ -28,18 +28,15 @@ def list_of_objects(file_name):
                     copied_array = copy.deepcopy(to_modify.data)
                     copied_array[index_var.data] = p["data"]
                     
-                    vis_list = Visual_List(list_name_stripped, copied_array, 0, 0)
+                    vis_list = Visual_List(list_name_stripped, copied_array, 0, 0, size = width / 12)
                     
                     vis_list.var_collection[index_var.data] = highlight(vis_list.var_collection[index_var.data])
 
-<<<<<<< HEAD
-                    objects.append(Visual_List(list_name_stripped, copied_array, 0, 0, size = width / 12))
-=======
                     objects.append(vis_list)
->>>>>>> 103497c89f510b7fbc6b7608d490a104fd7ecf14
+
                     #print(copied_array)
                 else:
-                    objects.append(Variable(p["name"], p["data"], 100, (3 * width / 32) * p["data"]), size = width / 12)
+                    objects.append(Variable(p["name"], p["data"], 100, (3 * width / 32) * p["data"], size = width / 12))
             if p["type"] == "<class 'string'>":
                 objects.append(Variable(p["name"], p["data"], 100, 100))
         return objects
